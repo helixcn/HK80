9f9602877fffe0d67552157b3a3ecfdea2e2a858
Jinlong Zhang <jinlongzhang01@gmail.com>
2021-05-06 09:42:27 +0800

    Code formatted using the styler package

--- R/HK1980GRID_TO_HK80UTM.R
+++ R/HK1980GRID_TO_HK80UTM.R
@@ -1,7 +1,7 @@
 HK1980GRID_TO_HK80UTM <-
-function(N, E){
-     ### The unit for N and E is meter
-     temp <- HK1980GRID_TO_HK80GEO(N, E)
-     res <- HK80GEO_TO_HK80UTM(temp$latitude, temp$longitude)
-     return(res)
-}
+  function(N, E) {
+    ### The unit for N and E is meter
+    temp <- HK1980GRID_TO_HK80GEO(N, E)
+    res <- HK80GEO_TO_HK80UTM(temp$latitude, temp$longitude)
+    return(res)
+  }
--- R/HK1980GRID_TO_WGS84GEO.R
+++ R/HK1980GRID_TO_WGS84GEO.R
@@ -1,7 +1,7 @@
 HK1980GRID_TO_WGS84GEO <-
-function(N, E){
+  function(N, E) {
     ### The unit for N and E is meters
     temp <- HK1980GRID_TO_HK80GEO(N, E)
-    res  <- HK80GEO_TO_WGS84GEO(temp$latitude, temp$longitude)
+    res <- HK80GEO_TO_WGS84GEO(temp$latitude, temp$longitude)
     return(res)
-}
+  }
